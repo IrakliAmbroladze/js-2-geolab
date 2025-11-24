c579b8e9a0a536104e9ce6b3ccd59851a9bb9b4e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-24 11:01:53 +0400

    feat: create loop that sums up even numbers till 20

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const userPassword = prompt("შეიყვანეთ პაროლი");
+/*const userPassword = prompt("შეიყვანეთ პაროლი");
 
 if (userPassword === "geolabreact123") {
   console.log("წვდომა დაშვებულია");
 } else {
   console.log("წვდომა აკრძალულია");
+}*/
+
+let sum = 0;
+for (let i = 1; i <= 20; i++) {
+  console.log("enter for loop");
+  if (i % 2 === 0) {
+    sum += i;
+  }
 }
+
+console.log(sum);
