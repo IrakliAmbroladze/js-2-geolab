096235b42c84c5a99b617d9e0f607dec5c46c0cd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-24 12:32:55 +0400

    feat: add balance checking addin substracting logic

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (userPassword === "geolabreact123") {
   console.log("წვდომა დაშვებულია");
 } else {
   console.log("წვდომა აკრძალულია");
-}*/
+}
 
 let sum = 0;
 for (let i = 1; i <= 20; i++) {
@@ -14,4 +14,34 @@ for (let i = 1; i <= 20; i++) {
   }
 }
 
-console.log(sum);
+console.log(sum);*/
+let balance = 1000;
+let userChoice;
+while (Number(userChoice) !== 4) {
+  userChoice = prompt(
+    "აირჩიე ოპერაცია:\n1 - შეამოწმე ბალანსი\n2 - შეტანა\n3 - გატანა\n4 - გამოსვლა",
+  );
+  switch (Number(userChoice)) {
+    case 1:
+      alert("შენი ბალანსია: " + balance);
+      break;
+    case 2:
+      let depositAmount = +prompt("შეიყვანეთ დასამატებელი თანხა:");
+      balance += depositAmount;
+      alert("განახლებული ბალანსი: " + balance);
+      break;
+    case 3:
+      let creditAmount = +prompt("შეიყვანეთ გასატანი თანხა:");
+      if (creditAmount > balance) {
+        alert("ბალანსზე არაა საკმარისი თანხა");
+      } else {
+        balance -= creditAmount;
+        alert("განახლებული ბალანსი: " + balance);
+      }
+      break;
+    case 4:
+      break;
+    default:
+      alert("შეყვანილი მონაცემი არაზუსტია");
+  }
+}
